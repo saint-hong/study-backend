c126383e427a928454d7555d593f9bc1637dab4f
hshkuber <hshkuber@vmkuber.myguest.virtualbox.org>
2023-11-01 18:37:56 +0900

    minitter api

---
+++ config.py
@@ -0,0 +1,13 @@
+# sqlalchemy test file
+## database connect information
+
+db = {
+    "user": "root",
+    "password": "hshkuber1234",
+    "host": "localhost",
+    "port": 3306,
+    "database": "minitter"
+}
+
+DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
+JWT_SECRET_KEY = 'adminadmin1234'
