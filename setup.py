3fa4e2542e2f89f9f3a4abe97b121d11308111fd
hshkuber <hshkuber@vmkuber.myguest.virtualbox.org>
2023-11-01 19:06:56 +0900

    minitter api

---
+++ setup.py
@@ -0,0 +1,20 @@
+# this is setup file for deploying minitter api in aws EC2 instance
+
+import sys
+
+from flask_script import Manager
+# import flask object from source code file : app.py
+from app import create_app
+from flask_twisted import Twisted
+from twisted.python import log
+
+if __name__ == "__main__" : 
+    app = create_app()
+    twisted = Twisted(app)
+    log.startLogging(sys.stdout)
+
+    app.logger.info(f"Running the app...")
+
+    manager = Manager(app)
+    manager.run()
+
